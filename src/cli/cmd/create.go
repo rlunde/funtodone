8228b831eb9cfff99ae2060abc66a81fe7e62ef0
Ron Lunde <rlunde@gmail.com>
2016-12-15 22:55:37 -0500

    stubbed out create/delete of user, task, and collection

--- src/cli/cmd/create.go
+++ src/cli/cmd/create.go
@@ -6,68 +6,68 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// deleteCmd represents the delete command
-var deleteCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "delete tasks, users, or collections of tasks",
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create tasks, users, or collections of tasks",
 	Long: `funtodone-cli is primarily a wrapper around library functions. Most
 	real users will only use it via the web app interface.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("delete called")
+		fmt.Println("create called")
 	},
 }
 
-// deleteUserCmd represents the delete user command
-var deleteUserCmd = &cobra.Command{
+// createUserCmd represents the create user command
+var createUserCmd = &cobra.Command{
 	Use:   "user",
-	Short: "delete a user",
+	Short: "create a user",
 	Long: `funtodone-cli is primarily a wrapper around library functions. Most
     real users will only use it via the web app interface.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("delete user called")
+		fmt.Println("create user called")
 	},
 }
 
-// deleteTaskCmd represents the delete task command
-var deleteTaskCmd = &cobra.Command{
+// createTaskCmd represents the create task command
+var createTaskCmd = &cobra.Command{
 	Use:   "task",
-	Short: "delete a task",
+	Short: "create a task",
 	Long: `funtodone-cli is primarily a wrapper around library functions. Most
     real users will only use it via the web app interface.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("delete task called")
+		fmt.Println("create task called")
 	},
 }
 
-// deleteCollectionCmd represents the delete collection command
-var deleteCollectionCmd = &cobra.Command{
+// createCollectionCmd represents the create collection command
+var createCollectionCmd = &cobra.Command{
 	Use:   "collection",
-	Short: "delete a collection",
+	Short: "create a collection",
 	Long: `funtodone-cli is primarily a wrapper around library functions. Most
     real users will only use it via the web app interface.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("delete collection called")
+		fmt.Println("create collection called")
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(deleteCmd)
-	deleteCmd.AddCommand(deleteUserCmd)
-	deleteCmd.AddCommand(deleteTaskCmd)
-	deleteCmd.AddCommand(deleteCollectionCmd)
+	RootCmd.AddCommand(createCmd)
+	createCmd.AddCommand(createUserCmd)
+	createCmd.AddCommand(createTaskCmd)
+	createCmd.AddCommand(createCollectionCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 }
