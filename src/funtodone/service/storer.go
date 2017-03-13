81c0643abaef050335f74c1ca0dca2952420cf0e
Ron Lunde <rlunde@gmail.com>
2017-03-12 22:17:22 -0400

    added an in-memory storer for a first pass

---
+++ src/funtodone/service/storer.go
@@ -0,0 +1,164 @@
+package service
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"gopkg.in/authboss.v1"
+)
+
+var nextUserID int
+
+//User is the user struct needed by authboss
+type User struct {
+	ID   int
+	Name string
+
+	// Auth
+	Email    string
+	Password string
+
+	// OAuth2
+	Oauth2Uid      string
+	Oauth2Provider string
+	Oauth2Token    string
+	Oauth2Refresh  string
+	Oauth2Expiry   time.Time
+
+	// Confirm
+	ConfirmToken string
+	Confirmed    bool
+
+	// Lock
+	AttemptNumber int64
+	AttemptTime   time.Time
+	Locked        time.Time
+
+	// Recover
+	RecoverToken       string
+	RecoverTokenExpiry time.Time
+
+	// Remember is in another table
+}
+
+//MemStorer is a temp storer struct to use until I get mongodb version working
+type MemStorer struct {
+	Users  map[string]User
+	Tokens map[string][]string
+}
+
+//NewMemStorer creates a dummy MemStorer struct
+func NewMemStorer() *MemStorer {
+	return &MemStorer{
+		Users: map[string]User{
+			"zeratul@heroes.com": User{
+				ID:        1,
+				Name:      "Zeratul",
+				Password:  "$2a$10$XtW/BrS5HeYIuOCXYe8DFuInetDMdaarMUJEOg/VA/JAIDgw3l4aG", // pass = 1234
+				Email:     "zeratul@heroes.com",
+				Confirmed: true,
+			},
+		},
+		Tokens: make(map[string][]string),
+	}
+}
+
+//Create adds a new user and an attribute to the MemStorer
+func (s MemStorer) Create(key string, attr authboss.Attributes) error {
+	var user User
+	if err := attr.Bind(&user, true); err != nil {
+		return err
+	}
+
+	user.ID = nextUserID
+	nextUserID++
+
+	s.Users[key] = user
+	fmt.Println("Create")
+	spew.Dump(s.Users)
+	return nil
+}
+
+//Put adds an attribute to the MemStorer
+func (s MemStorer) Put(key string, attr authboss.Attributes) error {
+	return s.Create(key, attr)
+}
+
+//Get returns an attribute from the MemStorer
+func (s MemStorer) Get(key string) (result interface{}, err error) {
+	user, ok := s.Users[key]
+	if !ok {
+		return nil, authboss.ErrUserNotFound
+	}
+
+	return &user, nil
+}
+
+//PutOAuth adds an OAuth attribute to the MemStorer
+func (s MemStorer) PutOAuth(uid, provider string, attr authboss.Attributes) error {
+	return s.Create(uid+provider, attr)
+}
+
+//GetOAuth returns an OAuth attribute from the MemStorer
+func (s MemStorer) GetOAuth(uid, provider string) (result interface{}, err error) {
+	user, ok := s.Users[uid+provider]
+	if !ok {
+		return nil, authboss.ErrUserNotFound
+	}
+
+	return &user, nil
+}
+
+func (s MemStorer) AddToken(key, token string) error {
+	s.Tokens[key] = append(s.Tokens[key], token)
+	fmt.Println("AddToken")
+	spew.Dump(s.Tokens)
+	return nil
+}
+
+func (s MemStorer) DelTokens(key string) error {
+	delete(s.Tokens, key)
+	fmt.Println("DelTokens")
+	spew.Dump(s.Tokens)
+	return nil
+}
+
+func (s MemStorer) UseToken(givenKey, token string) error {
+	toks, ok := s.Tokens[givenKey]
+	if !ok {
+		return authboss.ErrTokenNotFound
+	}
+
+	for i, tok := range toks {
+		if tok == token {
+			toks[i], toks[len(toks)-1] = toks[len(toks)-1], toks[i]
+			s.Tokens[givenKey] = toks[:len(toks)-1]
+			return nil
+		}
+	}
+
+	return authboss.ErrTokenNotFound
+}
+
+func (s MemStorer) ConfirmUser(tok string) (result interface{}, err error) {
+	fmt.Println("==============", tok)
+
+	for _, u := range s.Users {
+		if u.ConfirmToken == tok {
+			return &u, nil
+		}
+	}
+
+	return nil, authboss.ErrUserNotFound
+}
+
+func (s MemStorer) RecoverUser(rec string) (result interface{}, err error) {
+	for _, u := range s.Users {
+		if u.RecoverToken == rec {
+			return &u, nil
+		}
+	}
+
+	return nil, authboss.ErrUserNotFound
+}
