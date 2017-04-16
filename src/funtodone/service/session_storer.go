2130b912412641ea501369d1bd239050ececbd01
Ron Lunde <rlunde@gmail.com>
2017-04-15 22:00:21 -0400

    added some comments to make gometalinter happy

--- src/funtodone/service/session_storer.go
+++ src/funtodone/service/session_storer.go
@@ -12,15 +12,18 @@ const sessionCookieName = "ab_blog"
 
 var sessionStore *sessions.CookieStore
 
+//SessionStorer stores authorization in a session cookie
 type SessionStorer struct {
 	w http.ResponseWriter
 	r *http.Request
 }
 
+//NewSessionStorer returns an authboss ClientStorer
 func NewSessionStorer(w http.ResponseWriter, r *http.Request) authboss.ClientStorer {
 	return &SessionStorer{w, r}
 }
 
+//Get returns the value corresponding to a key and a boolean that says whether it was found
 func (s SessionStorer) Get(key string) (string, bool) {
 	session, err := sessionStore.Get(s.r, sessionCookieName)
 	if err != nil {
@@ -41,6 +44,7 @@ func (s SessionStorer) Get(key string) (string, bool) {
 	return str, true
 }
 
+//Put stores a value under a key
 func (s SessionStorer) Put(key, value string) {
 	session, err := sessionStore.Get(s.r, sessionCookieName)
 	if err != nil {
@@ -52,6 +56,7 @@ func (s SessionStorer) Put(key, value string) {
 	session.Save(s.r, s.w)
 }
 
+//Del deletes a value if found, but doesn't return an error if it wasn't
 func (s SessionStorer) Del(key string) {
 	session, err := sessionStore.Get(s.r, sessionCookieName)
 	if err != nil {
