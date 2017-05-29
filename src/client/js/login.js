74ca3578b2458d696ef66deb97cbde00a2facc75
Ron Lunde <rlunde@gmail.com>
2017-05-28 23:04:10 -0500

    trying to find a bug

--- src/client/js/login.js
+++ src/client/js/login.js
@@ -14,12 +14,12 @@ $(function() {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-  $('#register-').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
+  // $('#register-').click(function(e) {
+	// 	$("#register-form").delay(100).fadeIn(100);
+ // 		$("#login-form").fadeOut(100);
+	// 	$('#login-form-link').removeClass('active');
+	// 	$(this).addClass('active');
+	// 	e.preventDefault();
+	// });
 
 });
