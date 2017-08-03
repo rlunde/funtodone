c156031ceee6ab293163552426d69a8e4ead2daa
Ron Lunde <rlunde@gmail.com>
2017-08-02 21:56:27 -0400

    added tests for session insert, read, and update

--- src/funtodone/session/session_test.go
+++ src/funtodone/session/session_test.go
@@ -22,10 +22,26 @@ func TestSessionInitAndRead(t *testing.T) {
 	if err != nil {
 		t.Errorf("SessionInit failed: %s\n", err.Error())
 	}
+	// add something to the map
+	session.Set("email", "al@pa.ca")
+	email := session.Get("email")
+	if email != "al@pa.ca" {
+		t.Errorf("session should have email: %s but has email: %s", "al@pa.ca", email)
+	}
+	err = mgr.SessionUpdate(session)
+	if err != nil {
+		t.Errorf("SessionUpdate failed: %s\n", err.Error())
+	}
 	returnedSession := Session{SessionID: session.SessionID}
 	err = mgr.SessionRead(&returnedSession)
 	if err != nil {
 		t.Errorf("SessionRead failed: %s\n", err.Error())
 	}
-	// TODO: check contents
+	if session.SessionID != returnedSession.SessionID {
+		t.Errorf("SessionInit has SessionID: %s but returnedSession has SessionID: %s", session.SessionID, returnedSession.SessionID)
+	}
+	returnedEmail := returnedSession.Get("email")
+	if returnedEmail != email {
+		t.Errorf("loaded session has email: %s but should have email: %s", returnedEmail, email)
+	}
 }
