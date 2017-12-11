2ab785360be9e78c3c97bdf641880ee5a2519920
Ron Lunde <rlunde@gmail.com>
2017-12-10 22:35:44 -0500

    added flex layout

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from "react"
+import React, {Component} from "react"
 
 class App extends Component {
   render() {
     return (
-      <div>FunToDone</div>
+      <div>FunToDone
+        <header>Header</header>
+        <div className="funtodone-body">
+          <main className="funtodone-content">Main Panel</main>
+          <nav className="funtodone-nav">Nav Panel</nav>
+          <aside className="funtodone-aside">Aside</aside>
+        </div>
+        <footer>Footer</footer>
+      </div>
     )
   }
 }
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { createStore, applyMiddleware } from "redux"
 import "./style/index.css"
+import "./style/App.css"
 
 import App from "./components/App"
 import reducers from "./reducers"
